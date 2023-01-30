9922d5f0dc93f8bed1eca14a5cc299af199638c7
NawalKH <ezma1996@gmail.com>
2023-01-31 01:29:50 +0300

    Add front-end build

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 morgan.token('data', function (req, res) { return JSON.stringify(req.body)})
