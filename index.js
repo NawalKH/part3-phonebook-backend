49dcac866b57b5782ffebdf4ff0f8b81e74e53a5
NawalKH <ezma1996@gmail.com>
2023-02-27 01:26:02 +0300

    Save data to MangoDB

--- index.js
+++ index.js
@@ -1,74 +1,56 @@
+require('dotenv').config()
 const express = require('express')
 var morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 const app = express()
-
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
-app.use(express.static('build'))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 morgan.token('data', function (req, res) { return JSON.stringify(req.body)})
-
- let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
 
   app.get('/', (request, response) => {
     response.send('<h1>Phonebook Application</h1>')
   })
   
   app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+      response.json(persons)
+    })
   })
 
 
   app.get('/info', (request, response) => {
     const currentDate = new Date(); 
-    response.send(
-      `<p>phonebook has info for ${persons.length} people</p>
-        <p> ${currentDate} </p>`)
+    Person.find({}).then(persons => {
+       response.send(
+        `<p>phonebook has info for ${persons.length} people</p>
+          <p> ${currentDate} </p>`) })    
   })
 
   
-  app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
+  app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id).then(person => {
+      if (person) {
         response.json(person)
       } else {
         response.status(404).end()
       }
+    })
+    .catch(error => next(error))
   })
 
   
-  app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
-  })
-
+  app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
   
   const generateId = () => {
     return Math.floor(Math.random() * 10000);
@@ -82,27 +64,46 @@ morgan.token('data', function (req, res) { return JSON.stringify(req.body)})
         error: 'name or number is missing' 
       })
     }
-
-    const name = persons.find(person => person.name === body.name)
-    if (name) {
-      return response.status(400).json({ 
-        error: 'name must be unique'
-      })
-    }
   
-    const person = {
+    const person = new Person ({
       id: generateId(),
+      name: body.name,
+      number: body.number
+    })
+  
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
+  })
+
+  app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+ 
+    const person = {
       name: body.name,
       number: body.number
     }
   
-    persons = persons.concat(person)
-  
-    response.json(person)
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
   })
+
+   // ERROR HANDELING
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
   
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
   
-  const PORT = process.env.PORT || 3001
+    next(error)
+  }
+  app.use(errorHandler)
+  
+  const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
