b297eb0fb9eebaa68898104c2856a3deb23489de
NawalKH <ezma1996@gmail.com>
2023-02-27 20:57:57 +0300

    Fix phone number validation

--- models/person.js
+++ models/person.js
@@ -25,7 +25,7 @@ const personSchema = new mongoose.Schema({
     minLength: 8,
     validate: {
       validator: function (v) {
-        return /^[0-9]{2,3}[-]?\d/.test(v)
+        return /^\d{2,3}[-]?\d+$/.test(v)
       },
       message: 'Number is not valid'
     },
